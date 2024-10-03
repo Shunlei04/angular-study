01db688700fb771f1e21fff11664cdd8f11a8c1c
Shunlei04 <shunleikhinethin@gmail.com>
2024-10-03 15:58:46 +0630

    tsbasic, ngcomponent,flexbox

---
+++ src/app/apps/signin/signin.component.ts
@@ -0,0 +1,39 @@
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.scss',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
+})
+export class SigninComponent {
+  @Input() username: string = 'SLKT';
+
+  private route = inject(Router);
+
+  goToHome() {
+    this.route.navigate(['home'], { replaceUrl: false });
+  }
+}
---
+++ src/app/components/table-pages/table-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-table-pages',
+  templateUrl: './table-pages.component.html',
+  styleUrl: './table-pages.component.scss',
+  standalone: true,
+  imports: [MatButtonModule],
+})
+export class TablePagesComponent {
+  @Input() activePage: number = 4;
+  @Input() pageList: number[] = new Array(7);
+  constructor() {}
+}
